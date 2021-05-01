7e8f048a1e0d2e6a06216775001744eba105cde0
yoonsung <sbdyzjdla@gmail.com>
2021-05-01 23:56:40 +0900

    챕터2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,55 @@
+import domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            logic(em);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
     }
+
+    public static void logic(EntityManager em) {
+
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("지한");
+        member.setAge(2);
+        //등록
+        em.persist(member);
+
+        //수정
+        member.setAge(20);
+
+        //한 건 조회
+        Member findMember = em.find(Member.class, id);
+        System.out.println("find domain.Member Id : " + findMember.getId()
+        + ", member name " + findMember.getUsername()
+        + ", member age : " + findMember.getAge());
+
+        //목록 조회
+        List<Member> members = em.createQuery("select m from domain.Member m", Member.class)
+                .getResultList();
+        System.out.println("Members = " + members);
+        System.out.println("domain.Member.size = " + members.size());
+
+        em.remove(member);
+    }
+
 }
